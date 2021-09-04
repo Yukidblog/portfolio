51f01d95817b9f8b9e2daa85c930b93e8dae2622
Yukidblog <83527592+Yukidblog@users.noreply.github.com>
2021-09-04 15:09:48 +0900

    Add files via upload

--- gokuraku/js/script.js
+++ gokuraku/js/script.js
@@ -1,47 +1,49 @@
-
-$(function () {
-  $('#rec-slick').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    autoplay: true,
-    speed: 1000,
-    autoplaySpeed: 5000,
-    arrows: false,
-    centerPadding: '100px',
-    centerMode: true,
-    responsive: [
-      {
-      breakpoint: 767,
-      settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      centerPadding: 0,
-      centerMode: true,
-      }
-  }
-]
-  });
-});
-
-
-
-
+// Recommend / slick.js
+// ==================================================
 $(function(){
-  $('#nav-btn').on('click',function(){
-      $(this).toggleClass('-active');
-      $('#nav').toggleClass('-active');
-  });
+    $('#rec-slick').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        speed: 1000,
+        autoplaySpeed: 5000,
+        arrows: false,
+        centerPadding: '100px',
+        centerMode: true,
+            responsive: [
+                {
+                breakpoint: 767,
+                settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerPadding: 0,
+                centerMode: true,
+                }
+            }
+        ]
+    });
 });
 
-
-$('.faq-title').on('click', function(){
-  $(this).toggleClass('active');
-  $(this).next().slideToggle();
-});
-
+// header > nav
+// ==================================================
 $(function(){
-  $('.js-accordion').on('click',function(){
-    $(this).next().slideToggle();
-  })
-})
+    $('#nav-btn').on('click',function(){
+        $(this).toggleClass('-active');
+        $('#nav').toggleClass('-active');
+    });
+});
+
+// FAQ
+// ==================================================
+$(function(){
+    $('.js-accordion').on('click',function(){
+        $(this).next().slideToggle();
+    })
+});
+
+// animation / wow.js
+// ==================================================
+$(function(){
+    new WOW().init();
+});
